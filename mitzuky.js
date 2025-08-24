0733eb45ca1c00225aa4eae14a340e16d0bea6e4
Vibhuu-08 <vibhupathum29@gmail.com>
2025-08-25 02:42:43 +0530

    Update and rename queen.js to mitzuky.js

--- mitzuky.js
+++ mitzuky.js
@@ -29,7 +29,7 @@ const sess = require("./session");
 async function sessdl() {
   try {
     // Extract the Base64 encoded session data
-    const base64Data = sess.SESSION_ID.split("ANJU-XPRO~")[1];
+    const base64Data = sess.SESSION_ID.split("MITZUKY-BETA~")[1];
     if (!base64Data) {
       throw new Error("Invalid SESSION_ID format - missing Base64 data");
     }
@@ -91,7 +91,7 @@ async function connectToWA() {
   const getPrefix = () => config.PREFIX;
   const getWelcome = () => config.WELCOME;
   //===========================
-  console.log("🔥 QUEEN ANJU XPRO is starting...");
+  console.log("🔥 MitZuky Beta is starting...");
   const { state, saveCreds } = await useMultiFileAuthState(
     __dirname + "/sessions/"
   );
@@ -119,9 +119,9 @@ async function connectToWA() {
       console.log("connected to whatsapp ✅");
       // Assuming `config` contains all the settings
       let up = `
-          🚀 **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚 Connected Successfully!** ✅ 
+          🚀 **© MitZuky Beta 💚 Connected Successfully!** ✅ 
           
-          --- **🎉 Welcome to © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚!** 🎉 
+          --- **🎉 Welcome to © MitZuky Beta 💚!** 🎉 
           ✦» 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${require("./package.json").version}
           ✦» 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
           ✦» 𝙷𝚘𝚜𝚝 : ${os.hostname()}
@@ -150,7 +150,7 @@ async function connectToWA() {
             config.AUTORECORDING ? "Enabled" : "Disabled"
           }
       
-          --- Thank you for using **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚**. 
+          --- Thank you for using **© MitZuky Beta 💚**. 
           We're here to make your experience enjoyable and seamless. 
           If you need any help or have questions, don't hesitate to ask. 
           
@@ -159,22 +159,22 @@ async function connectToWA() {
       conn.sendMessage(conn.user.id, {
         text: up,
         contextInfo: {
-          mentionedJid: ["94717775628@s.whatsapp.net"], // specify mentioned JID(s) if any
+          mentionedJid: ["94721931393@s.whatsapp.net"], // specify mentioned JID(s) if any
           groupMentions: [],
           forwardingScore: 999,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
-            newsletterJid: "120363299978149557@newsletter",
-            newsletterName: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
+            newsletterJid: "",
+            newsletterName: "© MitZuky Beta 💚",
             serverMessageId: 999,
           },
           externalAdReply: {
-            title: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
-            body: " ©𝐌𝐑 𝐑𝐀𝐒𝐇𝐌𝐈𝐊𝐀 𝐎𝐅𝐂 💚",
+            title: "© MitZuky Beta 💚",
+            body: " © Vibhu Pathum 💚",
             mediaType: 1,
-            sourceUrl: "https://github.com/Mrrashmika",
+            sourceUrl: "",
             thumbnailUrl:
-              "https://raw.githubusercontent.com/RASH-DATA/ANJU-DATA/refs/heads/main/LOGOS/6152181515400889311.jpg", // This should match the image URL provided above
+              "https://i.ibb.co/nqyv38xB/20250824-170551.jpg", // This should match the image URL provided above
             renderLargerThumbnail: false,
             showAdAttribution: true,
           },
