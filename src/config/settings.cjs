620aa2ad846804c7d16e747ce0ce2301c22c0fbd
Vibhuu-08 <vibhupathum29@gmail.com>
2025-08-25 01:47:46 +0530

    Create settings.cjs

---
+++ src/config/settings.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "PREFIX": "$",
+  "OWNERS": [
+    "94721931393@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": false,
+  "MODE": "public",
+  "BOTNUMBER": "94721931393",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "Vibhu Pathum",
+  "NEWSJID": " ",
+  "MOVIEJID": false,
+  "ANIMEJID": "",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "🦦",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "94720255216"
+  ]
+};
